1f9d8fd5e69c93237148fd4a69855fc86bd41d07
0carvalh0 <trabalho.joaovictor2004@gmail.com>
2024-12-22 23:26:54 -0300

    fix: velocidade de rotação

--- ThreeJS/script.js
+++ ThreeJS/script.js
@@ -26,8 +26,8 @@ camera.position.z = 5;
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.03;
-  cube.rotation.y += 0.03;
+  cube.rotation.x += 0.02;
+  cube.rotation.y += 0.02;
 
   renderer.render(scene, camera);
 }
